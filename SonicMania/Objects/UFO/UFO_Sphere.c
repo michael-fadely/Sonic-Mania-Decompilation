88cac60b421b9bb0efd794d4bf8e7e7f42f72e13
jnmartin84 <jnmartin84@gmail.com>
2026-02-26 17:09:52 -0500

    depth is tricky on these

--- SonicMania/Objects/UFO/UFO_Sphere.c
+++ SonicMania/Objects/UFO/UFO_Sphere.c
@@ -62,7 +62,7 @@ void UFO_Sphere_Draw(void)
     Vector4f pos;
     pos.x = (float)self->drawPos.x;
     pos.y = (float)self->drawPos.y;
-    pos.z = shz_divf(65536.0f*0.5f, (float)self->zdepth);
+    pos.z = shz_divf(65536.0f, (float)self->zdepth);
 
     RSDK.Draw3DSprite(&self->animator, &pos, true);
 #else
